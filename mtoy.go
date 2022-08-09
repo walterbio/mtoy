e954af4d8a2bdbd7f402b259a407ed971f722dac
Walter Vargas <waltervargas@linux.com>
2022-08-10 01:09:45 +0200

    Add support for customizable list of answers

--- mtoy.go
+++ mtoy.go
@@ -1,3 +1,4 @@
+// mtoy implements Magic Ball 8 Game
 package mtoy
 
 import (
@@ -5,48 +6,40 @@ import (
 	"time"
 )
 
-var (
-	answers = []string{
-		"Ask again later.",
-		"As I see it, yes.",
-		"Better not tell you now.",
-		"Cannot predict now.",
-		"Concentrate and ask again.",
-		"Don't count on it.",
-		"It is certain.",
-		"It is decidedly so.",
-		"Most likely.",
-		"My reply is no.",
-		"My sources say no.",
-		"Outlook good.",
-		"Outlook not so good.",
-		"Reply hazy, try again.",
-		"Signs point to yes.",
-		"Very doubtful. ",
-		"Without a doubt.",
-		"Yes.",
-		"Yes definitely.",
-		"You may rely on it.",
-	}
-)
-
 type mtoy struct {
-	seed int64
+	seed    int64
+	answers []string
 }
 
-// to be called by the tests
-func New(seed int64) mtoy {
+// New returns a mtoy instance that can be used to customize the seed receives a
+// seed and an optional list of answers to be used instead of the default list
+// of answers.
+func New(seed int64, answers ...string) mtoy {
+	// TODO: is there another way to do this?
+	// to support custom answers and parallel tests I can't use a reference to
+	// default_answers variable
+	// NOTE: Copy copies the minimun of len(dst) and len(src), if you create a
+	// slice with cap: 0, no elements will be copied.
+	local_answers := make([]string, len(default_answers))
+	copy(local_answers, default_answers)
+
+	if len(answers) > 0 {
+		local_answers = answers
+	}
+
 	return mtoy{
-		seed: seed,
+		seed:    seed,
+		answers: local_answers,
 	}
 }
 
+// RevealAnswer returns a randomized answer based on Magic 8 Ball game
 func (m mtoy) RevealAnswer() string {
 	rand.Seed(m.seed)
-	return answers[rand.Intn(len(answers))]
+	return m.answers[rand.Intn(len(m.answers))]
 }
 
-// Default behaviour
+// RevealAnswer returns a randomized answer based on Magic 8 Ball game
 func RevealAnswer() string {
 	m := New(time.Now().Unix())
 	return m.RevealAnswer()
--- mtoy_test.go
+++ mtoy_test.go
@@ -43,7 +43,43 @@ func TestRevealAnswer2(t *testing.T) {
 	}
 }
 
-// how can we see the state of random generator without
-// accessing it?
-//
-//
+func TestRevealAnswerCustomAnswers(t *testing.T) {
+	t.Parallel()
+
+	answers := []string{
+		"Como yo lo veo, sí",
+		"Concéntrate y pregunta otra vez",
+		"Es cierto",
+		"Es decididamente así",
+		"Las señales apuntan a que sí",
+		"Más probable",
+		"Mejor no decirte ahora",
+		"Mi respuesta es no",
+		"Mis fuentes dicen que no",
+		"Muy dudoso",
+		"no cuentes con eso",
+		"No se puede predecir ahora",
+		"Perspectiva no tan buena",
+		"Perspectivas buena",
+		"Pregunta de nuevo más tarde",
+		"Puedes confiar en ello",
+		"Respuesta confusa, intenta otra vez",
+		"Sí",
+		"Sí definitivamente",
+		"Sin duda",
+	}
+
+	m := mtoy.New(42, answers...)
+	want := answers[5]
+	got := m.RevealAnswer()
+	if want != got {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+
+	m = mtoy.New(1, answers...)
+	want2 := answers[1]
+	got2 := m.RevealAnswer()
+	if want2 != got2 {
+		t.Errorf("got: %s, want: %s", got2, want2)
+	}
+}
